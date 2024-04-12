0c8c54da1766f266fe03a72084bc2d3090517ab4
IvanRulevskyi <rulevskiy4@gmail.com>
2024-04-12 16:50:13 +0300

    FinishProgram

---
+++ TwoDynamicArr/Function.cpp
@@ -0,0 +1,8 @@
+//
+//  Function.cpp
+//  TwoDynamicArr
+//
+//  Created by Иван Рулевский on 12.04.2024.
+//
+
+#include "Function.hpp"
---
+++ TwoDynamicArr/Function.hpp
@@ -0,0 +1,13 @@
+//
+//  Function.hpp
+//  TwoDynamicArr
+//
+//  Created by Иван Рулевский on 12.04.2024.
+//
+
+#ifndef Function_hpp
+#define Function_hpp
+
+#include <stdio.h>
+
+#endif /* Function_hpp */
